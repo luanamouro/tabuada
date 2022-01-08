35d5b48fdcfb81d567dc88bf7ab7c16ef43d1aaa
Luana <luana.mouro@gmail.com>
2022-01-08 20:25:17 -0300

    Tabuada

---
+++ tabuada/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace tabuada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero,res,i=1;
+            Console.WriteLine("Tabuada\n\n");
+            Console.WriteLine("Digite um número e veja a tabuada deste:");
+            numero = Convert.ToInt32(Console.ReadLine());
+
+            while (i <= 10)
+            {
+
+                res = numero * i;
+                Console.WriteLine("{0} x {1} = {2}", numero.ToString(), i.ToString(), res.ToString());
+                i++;
+
+
+            }
+        }
+    }
+}
